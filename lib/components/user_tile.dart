d04a40f83feed757987cb21d4f46e3345b5f3e00
Izraquel <https://github.com/Izraquel>
2023-01-19 17:31:42 -0300

    finalizado

---
+++ lib/components/user_tile.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud/provider/users.dart';
+import 'package:provider/provider.dart';
+
+import '../models/user.dart';
+import '../routes/app_routes.dart';
+
+class UserTile extends StatelessWidget {
+  final User user;
+
+  const UserTile(this.user, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final avatar = user.avatarUrl.isEmpty
+        ? const CircleAvatar(child: Icon(Icons.person))
+        : CircleAvatar(backgroundImage: NetworkImage(user.avatarUrl));
+    return ListTile(
+      leading: avatar,
+      title: Text(user.name),
+      subtitle: Text(user.email),
+      trailing: SizedBox(
+        width: 100,
+        child: Row(children: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.edit),
+            color: Colors.orange,
+            onPressed: () {
+              Navigator.of(context).pushNamed(
+                AppRoutes.USER_FORM,
+                arguments: user,
+              );
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.delete),
+            color: Colors.red,
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (ctx) => AlertDialog(
+                  title: const Text('Excluir usuário'),
+                  content: const Text('Tem certeza ???'),
+                  actions: <Widget>[
+                    TextButton(
+                      child: const Text('Não'),
+                      onPressed: () => Navigator.of(context).pop(false),
+                    ),
+                    TextButton(
+                      child: const Text('Sim'),
+                      onPressed: () => Navigator.of(context).pop(true),
+                    ),
+                  ],
+                ),
+              ).then((confirmed) => {
+                    if (confirmed)
+                      {Provider.of<Users>(context, listen: false).remove(user)}
+                  });
+            },
+          ),
+        ]),
+      ),
+    );
+  }
+}
---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,34 @@
+import '../models/user.dart';
+
+const DUMMY_USERS = {
+  '1': User(
+      id: '1',
+      name: 'Maria',
+      email: 'maria@gmail.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/04/25/21/14/woman-1353211_960_720.png'),
+  '2': User(
+      id: '2',
+      name: 'João',
+      email: 'joão@gmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/12/13/16/17/dancer-1904467_960_720.png'),
+  '3': User(
+      id: '3',
+      name: 'Fernanda',
+      email: 'fernanda@zymail.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/08/28/13/12/secondlife-1625903_960_720.jpg'),
+  '4': User(
+      id: '4',
+      name: 'Lucas',
+      email: 'lukinha@zymail.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'),
+  '5': User(
+      id: '5',
+      name: 'Raquel',
+      email: 'kekel@hotmail.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2021/05/10/08/00/woman-6242836_960_720.png'),
+};
